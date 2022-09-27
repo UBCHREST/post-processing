b4549a2e3af6e1e229d0b610d418ffabc548c413
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-09-27 12:46:19 -0400

    draft of the 2hdft to xdmfGenerator

---
+++ twoColorPyrometry/conv2hdf5.m
@@ -0,0 +1,44 @@
+
+
+filename = dir('temperature*.mat')
+for i=1:length(filename)
+    currFileName = filename(i).name;
+    file = currFileName(1:end-4);
+
+    matstruct=load(currFileName);
+    mat = vertcat(matstruct.flameTemp);
+
+    %     assume everything starts at 0
+    xstart=0;
+    ystart=0;
+    zstart=0;
+    %     assume discretization is uniform, dx=dy=dz, get dy from chamber height (1in)
+    dy=0.0254/size(mat,2);
+    dx=dy;
+    dz=dy;
+
+    xend=size(mat,1)*dx
+    yend=size(mat,2)*dy
+    zend=size(mat,3)*dz
+
+
+    %files to write
+    startm=[xstart ystart zstart]
+    endm=[xend yend zend]
+    dxm=[dx dy dz]
+
+    %     currFileName = 'myfile';
+    hdfname=append(file,'.h5')
+    if isfile(hdfname)==false
+        h5create(hdfname,'/main/tempdata',[414 83 20])
+        h5create(hdfname,'/main/start',[1 3])
+        h5create(hdfname,'/main/end',[1 3])
+        h5create(hdfname,'/main/discretization',[1 3])
+    end
+
+    h5write(hdfname,"/main/tempdata",flameTemp)
+    h5write(hdfname,"/main/start",startm)
+    h5write(hdfname,"/main/end",endm)
+    h5write(hdfname,"/main/discretization",dxm)
+
+end
---
+++ twoColorPyrometry/matlabToXdmfGenerator.py
@@ -0,0 +1,105 @@
+import argparse
+import pathlib
+import xml.etree.ElementTree as ET
+import h5py
+
+
+def write_data_item(xdmf_parent, hdf5_item, hdf5_filename):
+    dataItem = ET.SubElement(xdmf_parent, 'DataItem')
+    dataItem.set('Dimensions', ' '.join(map(str, hdf5_item.shape)))
+    dataItem.set('NumberType', 'Float')
+    dataItem.set('Precision', '8')
+    dataItem.set('Format', 'HDF')
+    dataItem.text = hdf5_filename + ":" + hdf5_item.name
+
+
+# generate the xdmf file xml for this file
+def generate_xdmf(hdf5_file, xdmf_file, root='main'):
+    # Load in the hdf5 file
+    hdf5 = h5py.File(hdf5_file, 'r')
+
+    # Check each of the cell fields
+    hdf5_fields = hdf5[root]
+
+    # store the dimensions, note this will be z, y, x
+    gridDim = []
+
+    # create a list of items to output
+    fieldNames = []
+    for field_name in hdf5_fields.keys():
+        # if field data
+        if hdf5_fields[field_name].ndim > 2:
+            fieldNames.append(field_name)
+            # check the dim
+            if gridDim:
+                if gridDim != hdf5_fields[field_name].shape:
+                    raise Exception("All fields must be of same size")
+            else:
+                gridDim = hdf5_fields[field_name].shape
+
+    # convert the gridDim to a string rep (note add one for cell/face info)
+    grid_cell_dim_string = ' '.join(map(lambda d: str(d + 1), gridDim))
+
+    # Create the root xdmf file element
+    xdmf = ET.Element('Xdmf')
+    domain = ET.SubElement(xdmf, 'Domain')
+    grid = ET.SubElement(domain, 'Grid')
+    grid.set('Name', 'mesh')
+    grid.set('GridType', 'Uniform')
+
+    # Set the baseline topology
+    topology = ET.SubElement(grid, 'Topology')
+    topology.set('TopologyType', '3DCoRectMesh')
+    topology.set('NumberOfElements', grid_cell_dim_string)
+
+    # add the simple geometry describing a structured grid
+    geometry = ET.SubElement(grid, 'Geometry')
+    geometry.set('GeometryType', 'Origin_DxDyDz')
+    write_data_item(geometry, hdf5_fields['start'], hdf5_file.name)
+    write_data_item(geometry, hdf5_fields['discretization'], hdf5_file.name)
+
+    # Add in the field data
+    for field_name in fieldNames:
+        fieldData = ET.SubElement(grid, 'Attribute')
+        fieldData.set('Name', field_name)
+        fieldData.set('AttributeType', 'Scalar')
+        fieldData.set('Center', 'Cell')
+        write_data_item(fieldData, hdf5_fields[field_name], hdf5_file.name)
+
+    # write to file
+    tree = ET.ElementTree(xdmf)
+    ET.indent(tree, space="\t", level=0)
+    with open(xdmf_file, 'w') as f:
+        f.write('<?xml version="1.0" ?>\n<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>\n')
+        tree.write(f, xml_declaration=False, encoding='unicode')
+
+
+def expand_path(path_pattern):
+    p = pathlib.Path(path_pattern)
+    return p.parent.expanduser().glob(p.name)
+
+
+# Main function to parser input files and run the document generator
+def parse():
+    parser = argparse.ArgumentParser(description='Generate an xdmf file from MatLab data files holding structed data. '
+                                                 'See https://github.com/cet-lab/experimental-post-processing/wiki'
+                                                 '/Matlab-To-XdmfGenerator for details.  ')
+    parser.add_argument('--file', dest='hdf5_file', type=pathlib.Path, required=True,
+                        help='The path to the hdf5 file(s) containing the structured data.  A wild card can be used to supply more than one file.')
+    args = parser.parse_args()
+
+    # expand any wild cards in the path name
+    hdf5_paths = expand_path(args.hdf5_file)
+
+    # convert with path
+    for hdf5_file in hdf5_paths:
+        # based upon the input file get the output file
+        xdmf_file = hdf5_file.parent / (hdf5_file.stem + ".xdmf")
+
+        # create component markdown
+        generate_xdmf(hdf5_file, xdmf_file)
+
+
+# parse based upon the supplied inputs
+if __name__ == "__main__":
+    parse()
