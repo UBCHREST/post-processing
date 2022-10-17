14f9ba6500d89fa235389fd784f610758159aed7
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-10-17 13:16:42 -0400

    add multi struct support

--- twoColorPyrometry/conv2hdf5_2D.m
+++ twoColorPyrometry/conv2hdf5_2D.m
@@ -3,44 +3,53 @@
 filename = dir('*.mat')
 for i=1:length(filename)
     currFileName = filename(i).name;
-    
+
     file = currFileName(1:end-4);
 
     matstruct=load(currFileName);
-    mat = vertcat(matstruct.intensity);
 
-    %     assume everything starts at 0
-    xstart=0;
-    ystart=0;
-    zstart=0;
-    %     assume discretization is uniform, dx=dy=dz, get dy from chamber height (1in)
-    dy=0.0254/83;
-    dx=dy;
-    dz=dy;
-
-    xend=size(mat,1)*dx
-    zend=size(mat,3)*dz
+    % march over each field
+    fields = fieldnames(matstruct);
 
 
-    %files to write
-    startm=[xstart zstart]
-    endm=[xend zend]
-    dxm=[dx dz]
+    for k=1:numel(fields)
+        if( isnumeric(matstruct.(fields{k})) )
+            mat = vertcat(matstruct.(fields{k}));
 
-    %     currFileName = 'myfile';
-    hdfname=append(file,'.h5')
-    if isfile(hdfname)==false
-        h5create(hdfname,'/main/intensity',[size(mat,1) size(mat,2)])
-        h5create(hdfname,'/main/start',[1 2])
-        h5create(hdfname,'/main/end',[1 2])
-        h5create(hdfname,'/main/discretization',[1 2])
+            %     assume everything starts at 0
+            xstart=0;
+            ystart=0;
+            zstart=0;
+            %     assume discretization is uniform, dx=dy=dz, get dy from chamber height (1in)
+            dy=0.0254/size(mat,2);
+            dx=dy;
+            dz=dy;
+
+            xend=size(mat,1)*dx
+            zend=size(mat,3)*dz
+
+
+            %files to write
+            startm=[xstart zstart]
+            endm=[xend zend]
+            dxm=[dx dz]
+
+            %     currFileName = 'myfile';
+            hdfname=append(file,'.h5');
+            hdf5FieldName = append('/main/', fields{k});
+            if isfile(hdfname)==false
+                h5create(hdfname,hdf5FieldName,[size(mat,1) size(mat,2)])
+                h5create(hdfname,'/main/start',[1 2])
+                h5create(hdfname,'/main/end',[1 2])
+                h5create(hdfname,'/main/discretization',[1 2])
+            end
+
+            h5write(hdfname,hdf5FieldName,mat)
+            h5write(hdfname,"/main/start",startm)
+            h5write(hdfname,"/main/end",endm)
+            h5write(hdfname,"/main/discretization",dxm)
+        end
     end
-
-    h5write(hdfname,"/main/intensity",mat)
-    h5write(hdfname,"/main/start",startm)
-    h5write(hdfname,"/main/end",endm)
-    h5write(hdfname,"/main/discretization",dxm)
-
 end
 
 % h5disp("temperature1.h5")
