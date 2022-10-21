949080017955ce434331fc36ffbdbc55ffaeacf5
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-10-21 08:46:49 -0400

    updated regression rate with output and avg

--- ablate/regression_rate/regressionRate.py
+++ ablate/regression_rate/regressionRate.py
@@ -1,7 +1,11 @@
 import argparse
 import math
 import pathlib
+import sys
+
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy
 
 from support.curveFileReader import parse_curve_file
 from support.supportPaths import expand_path
@@ -18,13 +22,17 @@ if __name__ == "__main__":
                         help='If a plot is generated for each time in the time series')
     args = parser.parse_args()
 
+    # Get the csv path
+    csv_path = args.curve_file.parent / "stats.csv"
+
     # create an empty data frame
-    regression_rates_rms = None
+    regression_rates_stats = None
 
     # Set some values
     xAxis = 'x'
     regressionMassFluxName = 'slab boundary_monitor_regressionMassFlux'
     regressionRateName = 'regression_rate mm/s'
+    regressionRateAvgName = 'regression_rate_avg'
     regressionRateRmsName = 'regression_rate_rms'
 
     # expand any wild cards in the path name
@@ -51,20 +59,32 @@ if __name__ == "__main__":
 
         # merge them together
         count += 1
-        if regression_rates_rms is not None:
+        if regression_rates_stats is not None:
             # else we need to merge
-            regression_rates_rms[regressionRateName] = regression_rates[regressionRateName]
-            regression_rates_rms[regressionRateRmsName] = regression_rates_rms.apply(
+            regression_rates_stats[regressionRateName] = regression_rates[regressionRateName]
+            regression_rates_stats[regressionRateAvgName] = regression_rates_stats.apply(
+                lambda row: row[regressionRateName] + row[regressionRateAvgName], axis=1)
+            regression_rates_stats[regressionRateRmsName] = regression_rates_stats.apply(
                 lambda row: row[regressionRateName] ** 2 + row[regressionRateRmsName], axis=1)
 
         else:
-            regression_rates_rms = regression_rates.filter([xAxis], axis=1)
-            regression_rates_rms[regressionRateRmsName] = regression_rates.apply(
+            regression_rates_stats = regression_rates.filter([xAxis], axis=1)
+            regression_rates_stats[regressionRateAvgName] = regression_rates.apply(
+                lambda row: row[regressionRateName], axis=1)
+            regression_rates_stats[regressionRateRmsName] = regression_rates.apply(
                 lambda row: row[regressionRateName] ** 2, axis=1)
 
     # finish the rms
-    regression_rates_rms[regressionRateRmsName] = regression_rates_rms.apply(
-        lambda row: math.sqrt(row[regressionRateRmsName] / count), axis=1)
-    regression_rates_rms_sorted = regression_rates_rms.sort_values(by=[xAxis])
+    regression_rates_stats[regressionRateAvgName] = regression_rates_stats.apply(
+        lambda row: row[regressionRateAvgName] / count, axis=1)
+    regression_rates_stats[regressionRateRmsName] = regression_rates_stats.apply(
+        lambda row: math.sqrt(
+            row[regressionRateRmsName] / count - row[regressionRateAvgName] * row[regressionRateAvgName]), axis=1)
+    regression_rates_rms_sorted = regression_rates_stats.sort_values(by=[xAxis])
     regression_rates_rms_sorted.plot.line(x='x', y=regressionRateRmsName, title=f'Regression Rate RMS')
+    regression_rates_rms_sorted.plot.line(x='x', y=regressionRateAvgName, title=f'Regression Rate Avg. mm/s')
+
+    print("Avg Reg Rate mm/s ", numpy.average(regression_rates_stats[regressionRateAvgName]))
+
+    pd.DataFrame(regression_rates_stats).to_csv(csv_path)
     plt.show()
