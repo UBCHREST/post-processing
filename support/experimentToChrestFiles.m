dd5d635fe20d73e29eea4c2fa459dc1fbb29c9b8
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-01-30 15:55:05 -0500

    setup mat lab for chrest file format (#4)
    * added code for generic chrest files from experiments
    
    * added code for generic chrest files from experiments

---
+++ support/experimentToChrestFiles.m
@@ -0,0 +1,101 @@
+% this support script is desinged to convert .m raw files from experiments
+% to the chrest hdf5 file format.  
+
+% update the file metadata 
+metadata=dictionary();
+metadata('fuel')="pmma";
+metadata('oxidizer')="O2";
+metadata('caseLabel')="20.55G";
+metadata('date')='2023-01-23';
+metadata('contact')="elektrak@buffalo.edu,kolosret@buffalo.edu";
+
+% specify the time history data
+timeStart=0.0;
+snapShotDt=1.0/1000;
+
+% update the grid information.  Specify the lower left hand corner of the
+% data. The start/end points are used to determine it is 1,2, or 3D
+startPoint=[0.01880658333, -0.01164166666582, -5E-3];
+endPoint=[0.1101, 0.0762, 0.0106];
+
+%%%%%%%%% Nothing below here needs to be changed
+matLabFiles = dir('*.mat');
+for i=1:length(matLabFiles)
+    % Get the file information
+    matLabFile = matLabFiles(i).name;
+    rawFileName= matLabFile(1:end-4);
+    chrestFile = append(rawFileName, '.h5');
+    newFile = ~ isfile(chrestFile);
+
+    % load the matlab data 
+    matStruct=load(matLabFile);
+
+    % march over each field in the matStruct
+    fields = fieldnames(matStruct);
+
+    % for each field
+    for k=1:numel(fields)
+        % make sure the field is data/number type
+        if( isnumeric(matStruct.(fields{k})) )
+            % Get the specific matlab mat
+            mat = vertcat(matStruct.(fields{k}));
+
+            % Compute delta x y z based upon the field data
+            delta = minus(endPoint, startPoint);
+            matSize=zeros(1,numel(delta));
+            for d=1:numel(delta)
+               delta(d) = delta(d)/size(mat,d);
+               matSize(d) = size(mat,d);
+            end
+
+            % Flip x to point in the same direction as numerical results
+            mat = flip(mat, 1);
+
+            % compute the field name
+            hdf5FieldName = ['/data/fields/' fields{k}];
+            
+            % Write the data
+            if newFile
+                h5create(chrestFile,hdf5FieldName,matSize);
+            end
+            h5write(chrestFile,hdf5FieldName,mat);
+        end
+    end
+
+    % create the grid information, it is assumed to be the same for all
+    % fields
+    if newFile
+      h5create(chrestFile,'/data/grid/start',[1 numel(delta)]);
+      h5create(chrestFile,'/data/grid/end',[1 numel(delta)]);
+      h5create(chrestFile,'/data/grid/discretization',[1 numel(delta)]);
+    end
+
+    % write the grid data, this may override what we have, but it
+    % should be the same
+    h5write(chrestFile,'/data/grid/start',startPoint);
+    h5write(chrestFile,'/data/grid/end',endPoint);
+    h5write(chrestFile,'/data/grid/discretization',delta);
+    % Write the metadata
+    metadataKeys=keys(metadata);
+    for m = 1:length(metadataKeys)
+        key = metadataKeys{m};
+        value = metadata(key);
+
+       if newFile
+            h5create(chrestFile,['/metadata/' key],1, 'Datatype','string');
+       end
+       h5write(chrestFile,['/metadata/' key],value)
+    end
+
+    % Compute and save the current time
+    indexString  = regexp(rawFileName, '(\d+)(?!.*\d)', 'match');
+    index=str2double(indexString);
+    time = timeStart+index*snapShotDt;
+    
+    % write the file
+    if newFile
+        h5create(chrestFile,'/time',1, 'Datatype','double');
+    end
+    h5write(chrestFile,'/time/',time)
+    
+end
---
+++ support/xdmfGenerator.py
@@ -0,0 +1,157 @@
+import argparse
+import pathlib
+import xml.etree.ElementTree as ET
+import h5py
+
+from support.supportPaths import expand_path
+
+class XdmfGenerator:
+    # The xdmf root
+    xdmf = ET.Element('Xdmf')
+
+    # the grid root, holds the times series
+    gridCollection = None
+
+    # The time history
+    timeHistory = []
+
+    def __init__(self):
+        # setup the root datastructure
+        domain = ET.SubElement(self.xdmf, 'Domain')
+        self.gridCollection = ET.SubElement(domain, 'Grid')
+
+        # set this grid to be a time seris
+        self.gridCollection.set('CollectionType', 'Temporal')
+        self.gridCollection.set('GridType', 'Collection')
+        self.gridCollection.set('Name', 'TimeSeries')
+
+    def append_chrest_hdf5(self, hdf5_file):
+        # Load in the hdf5 file
+        hdf5 = h5py.File(hdf5_file, 'r')
+
+        # Check each of the cell fields
+        hdf5_data = hdf5['data']
+
+        # store the dimensions, note this will be z, y, x
+        gridDim = []
+
+        # get the fields
+        hdf5_fields = hdf5_data['fields']
+        hdf5_grid = hdf5_data['grid']
+
+        # create a list of items to output
+        fieldNames = []
+        for field_name in hdf5_fields.keys():
+            # if field data
+            if hdf5_fields[field_name].ndim > 1 and hdf5_fields[field_name].shape[1] > 1:
+                fieldNames.append(field_name)
+                # check the dim
+                if gridDim:
+                    if gridDim != hdf5_fields[field_name].shape:
+                        raise Exception("All fields must be of same size")
+                else:
+                    gridDim = hdf5_fields[field_name].shape
+
+        # convert the gridDim to a string rep (note add one for cell/face info)
+        grid_cell_dim_string = ' '.join(map(lambda d: str(d + 1), gridDim))
+
+        # determine the dim of the grid
+        grid_dim = len(gridDim)
+
+        # create a new sub grid
+        grid = ET.SubElement(self.gridCollection, 'Grid')
+        grid.set('Name', 'mesh')
+        grid.set('GridType', 'Uniform')
+
+        # Set the baseline topology
+        topology = ET.SubElement(grid, 'Topology')
+        topology.set('TopologyType', topology_type[grid_dim])
+        topology.set('NumberOfElements', grid_cell_dim_string)
+
+        # add the simple geometry describing a structured grid
+        geometry = ET.SubElement(grid, 'Geometry')
+        geometry.set('GeometryType', geometry_type[grid_dim])
+        write_data_item(geometry, hdf5_grid['start'], hdf5_file.name)
+        write_data_item(geometry, hdf5_grid['discretization'], hdf5_file.name)
+
+        # Add in the field data
+        for field_name in fieldNames:
+            fieldData = ET.SubElement(grid, 'Attribute')
+            fieldData.set('Name', field_name)
+            fieldData.set('AttributeType', 'Scalar')
+            fieldData.set('Center', 'Cell')
+            write_data_item(fieldData, hdf5_fields[field_name], hdf5_file.name)
+
+        # Store the time
+        self.timeHistory.append(hdf5['time'][0])
+
+    def write_to_file(self, xdmf_file):
+        # add the full time history
+        timeHistory = ET.SubElement(self.gridCollection, 'Time')
+        timeHistory.set('TimeType', 'List')
+
+        timeArray = ET.SubElement(timeHistory, 'DataItem')
+        timeArray.set('Dimensions', str(len(self.timeHistory)))
+        timeArray.set('Format', 'XML')
+        timeArray.set('NumberType', 'Float')
+        timeArray.text = ' '.join(str(e) for e in self.timeHistory)
+
+        # write to file
+        tree = ET.ElementTree(self.xdmf)
+        ET.indent(tree, space="\t", level=0)
+        with open(xdmf_file, 'w') as f:
+            f.write('<?xml version="1.0" ?>\n<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>\n')
+            tree.write(f, xml_declaration=False, encoding='unicode')
+
+
+def write_data_item(xdmf_parent, hdf5_item, hdf5_filename):
+    dataItem = ET.SubElement(xdmf_parent, 'DataItem')
+    dataItem.set('Dimensions', ' '.join(map(str, hdf5_item.shape)))
+    dataItem.set('NumberType', 'Float')
+    dataItem.set('Precision', '8')
+    dataItem.set('Format', 'HDF')
+    dataItem.text = hdf5_filename + ":" + hdf5_item.name
+
+
+# store some dimensional information
+topology_type = {
+    2: "2DCoRectMesh",
+    3: "3DCoRectMesh"
+}
+
+geometry_type = {
+    2: "Origin_DxDy",
+    3: "Origin_DxDyDz"
+}
+
+
+# Main function to parser input files and run the document generator
+def parse():
+    parser = argparse.ArgumentParser(description='Generate an xdmf file from MatLab data files holding structed data. '
+                                                 'See https://github.com/cet-lab/experimental-post-processing/wiki'
+                                                 '/Matlab-To-XdmfGenerator for details.  ')
+    parser.add_argument('--file', dest='hdf5_file', type=pathlib.Path, required=True,
+                        help='The path to the hdf5 file(s) containing the structured data.  A wild card can be used '
+                             'to supply more than one file.')
+    args = parser.parse_args()
+
+    # Get the parent file
+    xdmf_file = args.hdf5_file.parent / (args.hdf5_file.stem.replace('*', '') + ".xdmf")
+
+    # expand any wild cards in the path name
+    hdf5_paths = expand_path(args.hdf5_file)
+
+    # generate an xdfm object
+    xdfm = XdmfGenerator()
+
+    # convert with path
+    for hdf5_file in hdf5_paths:
+        # create component markdown
+        xdfm.append_chrest_hdf5(hdf5_file)
+
+    # write the xdmf file
+    xdfm.write_to_file(xdmf_file)
+
+# parse based upon the supplied inputs
+if __name__ == "__main__":
+    parse()
