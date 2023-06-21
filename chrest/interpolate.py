de4b3c2a0884cc2446c511e89e68eb105fa50f43
kolosret <95316526+kolosret@users.noreply.github.com>
2023-06-21 14:56:25 -0400

    Update interpolate.py
    adding closest point when weights are negative

--- chrest/interpolate.py
+++ chrest/interpolate.py
@@ -15,24 +15,28 @@ import h5py
 import csv
 from multiprocessing import Pool
 
-def interp(values, vtx, wts, fill_value=np.nan):
+def interp(values, vtx, wts, points, fill_value=np.nan):
     ret = np.einsum('nj,nj->n', np.take(values, vtx), wts)
-    ret[np.any(wts < -1e10, axis=1)] = fill_value
+    mask=np.any(wts < 0, axis=1)
+    for i in range(len(ret)):
+        if mask[i]:
+            ret[i]=values[points[i]]
+    # ret[np.any(wts < -1e10, axis=1)] = fill_value
     return ret
 
 def find_simplex_piece(args):
     piece, tri = args
     return tri.find_simplex(piece)
 
-def calcweight3D(xyz,uvw):
+def calcweight3D(xyz,uvw,path):
 
     
     def interp_weights(xyz, uvw, d=3):
-        start_time = time.time()
+        # start_time = time.time()
         tri = qhull.Delaunay(xyz)
-        print("--- %s seconds for triangulation ---" % (time.time() - start_time))
+        # print("--- %s seconds for triangulation ---" % (time.time() - start_time))
         
-        start_time = time.time()
+        # start_time = time.time()
         
         from multiprocessing import cpu_count
         n_cores = cpu_count()
@@ -42,34 +46,36 @@ def calcweight3D(xyz,uvw):
         with Pool(processes=n_cores) as pool:
             results = pool.map(find_simplex_piece, [(piece, tri) for piece in pieces])
         simplex = np.concatenate(results)
-        print("--- %s seconds for finding simplices containing the points ---" % (time.time() - start_time))
+        # print("--- %s seconds for finding simplices containing the points ---" % (time.time() - start_time))
         
-        start_time = time.time()
+        # start_time = time.time()
         vertices = np.take(tri.simplices, simplex, axis=0)
-        print("--- %s seconds for getting the vertices from triangulation ---" % (time.time() - start_time))
+        # print("--- %s seconds for getting the vertices from triangulation ---" % (time.time() - start_time))
         
-        start_time = time.time()    
+        # start_time = time.time()    
         temp = np.take(tri.transform, simplex, axis=0)
-        print("--- %s seconds mapping triangulation into barycentric coord ---" % (time.time() - start_time))
+        # print("--- %s seconds mapping triangulation into barycentric coord ---" % (time.time() - start_time))
         
-        start_time = time.time()
+        # start_time = time.time()
         delta = uvw - temp[:, d]
-        print("--- %s seconds calculating local mapping coordinates ---" % (time.time() - start_time))
+        # print("--- %s seconds calculating local mapping coordinates ---" % (time.time() - start_time))
         
-        start_time = time.time()
+        # start_time = time.time()
         bary = np.einsum('njk,nk->nj', temp[:, :d, :], delta)
-        print("--- %s seconds getting the normalized barycentric coords ---" % (time.time() - start_time))
+        # print("--- %s seconds getting the normalized barycentric coords ---" % (time.time() - start_time))
         
         return vertices, np.hstack((bary, 1 - bary.sum(axis=1, keepdims=True)))    
 
-    start_time = time.time()
+    # start_time = time.time()
     # vtx, wts = interp_weights(xyz, uvww)
     
     filename = 'linweight3D_ablate_'+str(len(xyz))+'chrest_'+str(len(uvw))+'.csv'
     # filename2 = 'verticies_ablate_'+str(len(xyz))+'chrest_'+str(len(uvw))+'.csv'
-    if not os.path.exists(filename):
+    filepath = path / filename
+    if not filepath.exists():
         vtx, wts = interp_weights(xyz, uvw)
-        with open(filename, 'w', newline='') as csvfile:
+        print('Solving for weights')
+        with open(filepath, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(['Vertex 1', 'Vertex 2', 'Vertex 3','Vertex 4', 'Weight 1', 'Weight 2', 'Weight 3', 'Weight 4'])
             for i in range(len(vtx)):
@@ -77,8 +83,8 @@ def calcweight3D(xyz,uvw):
                 writer.writerow(row)
         print(f'Saved {filename} successfully.')
     else:
-
-        with open(filename, 'r') as csvfile:
+        print('Reading in precomputed weights')
+        with open(filepath, 'r') as csvfile:
             reader = csv.reader(csvfile)
             rows = list(reader)[1:]  # Skip the header row
             
@@ -87,16 +93,16 @@ def calcweight3D(xyz,uvw):
         wts = np.array([[float(row[i]) for i in range(4, 8)] for row in rows], dtype=np.float64)
         print(f'Read {filename} successfully.')
         
-    print("--- %s seconds for calculating the weights---" % (time.time() - start_time))
+    # print("--- %s seconds for calculating the weights---" % (time.time() - start_time))
     
     return vtx,wts
         
-def interpolate3D(vtx,wts,field):       
+def interpolate3D(vtx,wts,field,points):       
         
-    start_time = time.time()
+    # start_time = time.time()
     # valuesi = interpolate(values.flatten(), vtx, wts)
-    valuesi=interp(field, vtx, wts)
-    print("--- %s seconds for interpolation with the precalc weights---" % (time.time() - start_time))
+    valuesi=interp(field, vtx, wts,points)
+    # print("--- %s seconds for interpolation with the precalc weights---" % (time.time() - start_time))
     
     return valuesi
     
